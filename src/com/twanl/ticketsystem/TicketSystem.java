244c380370376de24367324febc86745d8ec2c29
Twan L <twanluttik@gmail.com>
2018-01-23 11:52:43 +0100

    added mySql

--- src/com/twanl/ticketsystem/TicketSystem.java
+++ src/com/twanl/ticketsystem/TicketSystem.java
@@ -8,6 +8,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TicketSystem extends JavaPlugin {
 
     protected PluginDescriptionFile pdfFile = getDescription();
@@ -15,9 +19,15 @@ public class TicketSystem extends JavaPlugin {
     private final String PluginVersionOff = ChatColor.RED + "(" + pdfFile.getVersion() + ")";
     public ConfigManager cfgM;
 
+    private Connection connection;
+    public String host, database, username, password;
+    public int port;
+
+
     public void onEnable() {
         loadConfigManager();
         LOAD();
+        mysqlSetup();
 
         Bukkit.getConsoleSender().sendMessage(Strings.logName + "Has been enabled " + PluginVersionOn);
     }
@@ -46,5 +56,41 @@ public class TicketSystem extends JavaPlugin {
         cfgM.setup();
         cfgM.savePlayers();
         cfgM.reloadplayers();
+    }
+
+    public void mysqlSetup() {
+        host = "localhost";
+        port = 3306;
+        database = "tickets";
+        username = "root";
+        password = "password";
+
+        try {
+            synchronized (this) {
+                if (getConnection() != null && !getConnection().isClosed()) {
+                    return;
+                }
+
+                Class.forName("com.mysql.jdbc.Driver");
+                setConnection( DriverManager.getConnection("jdbc:mysql://" + this.host + ":"
+                        + this.port + "/" + this.database, this.username, this.password ));
+
+                Bukkit.getConsoleSender().sendMessage(Strings.green + "MYSQL CONNECTED");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
     }
 }
