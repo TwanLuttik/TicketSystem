314f40ae2f57ff78fc55dedcbe25981233e5a745
BuildTools <unconfigured@null.spigotmc.org>
2018-01-31 15:32:10 +0100

    gonna use file insteal of sqlDatabase

--- src/com/twanl/ticketsystem/Enum.java
+++ src/com/twanl/ticketsystem/Enum.java
@@ -1,4 +1,31 @@
 package com.twanl.ticketsystem;
 
-public class Enum {
+
+public enum Enum {
+
+
+    ASSIGNED,  CLOSED,  OPEN;
+
+    private Enum() {}
+
+
+    /*
+    public enum get {
+        //ADD CUSTOMPOTION
+        BUG, REPORT, OTHER
+    }
+
+    static get CO = get.BUG;
+
+    public static void setTYPE(final Player p, get Getir){
+        switch(Getir){
+            case BUG:
+                break;
+            case REPORT:
+                break;
+            case OTHER:
+                break;
+        }
+    }
+    */
 }
--- src/com/twanl/ticketsystem/TicketSystem.java
+++ src/com/twanl/ticketsystem/TicketSystem.java
@@ -7,9 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class TicketSystem extends JavaPlugin {
 
@@ -17,14 +14,11 @@ public class TicketSystem extends JavaPlugin {
     private final String PluginVersionOn = ChatColor.GREEN + "(" + pdfFile.getVersion() + ")";
     private final String PluginVersionOff = ChatColor.RED + "(" + pdfFile.getVersion() + ")";
 
-    private Connection connection;
-    public String host, database, username, password, table;
-    public int port;
 
 
     public void onEnable() {
         LOAD();
-        mysqlSetup();
+        //mysqlSetup();
         loadConfig();
 
 
@@ -46,44 +40,6 @@ public class TicketSystem extends JavaPlugin {
         // Register commands
         Commands commands = new Commands();
         getCommand("ticket").setExecutor((CommandExecutor) commands);
-    }
-
-
-    public void mysqlSetup() {
-        host = this.getConfig().getString("host");
-        port = this.getConfig().getInt("port");
-        database = this.getConfig().getString("database");
-        username = this.getConfig().getString("username");
-        password = this.getConfig().getString("password");
-        table = this.getConfig().getString("table");
-
-        try {
-            synchronized (this) {
-                if (getConnection() != null && !getConnection().isClosed()) {
-                    return;
-                }
-
-                Class.forName("com.mysql.jdbc.Driver");
-                setConnection(
-                        DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database,
-                                this.username, this.password));
-
-                Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "MYSQL CONNECTED");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
     }
 
     public void loadConfig(){
